68e1b1b58617af63205c8964ffebc1b9aa0babbf
Ivan Zaera Avellon <izaera@gmail.com>
2016-02-03 21:51:20 +0100

    Show call stack in kernel panic

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -6,6 +6,11 @@
 #include "mmu.h"
 #include "multiboot.h"
 
+typedef struct _stack_frame_t {
+  struct _stack_frame_t* next;
+  void* ret;
+} stack_frame_t;
+
 extern uint8_t stack_bottom, stack_top;
 
 static void *kernel_start = (void*) 0x100000;
@@ -57,10 +62,8 @@ void main(multiboot_info_t* mbi, uint32_t magic) {
 }
 
 void k_panic(const char* msg) {
-  bios_setcolor(COLOR_WHITE, COLOR_RED);
-  bios_clrscr();
-  printf("\n\n\n\n\n\n\n     ");
-  printf("KERNEL PANIC: %s\n", msg);
+  bios_print("\nkernel panic: ");
+  bios_print(msg);
   asm volatile (
       "\tcli\n"
       ".Lhang:\n"
@@ -68,3 +71,38 @@ void k_panic(const char* msg) {
       "\tjmp .Lhang\n"
   );
 }
+
+size_t k_get_call_stack(size_t max_depth, void** return_addresses) {
+  register stack_frame_t* fp asm("ebp");
+  stack_frame_t* frame = fp;
+  size_t depth;
+
+  for (depth = 0; (depth < max_depth) && frame; depth++) {
+    return_addresses[depth] = frame->ret;
+    frame = frame->next;
+  }
+
+  return depth;
+}
+
+void k_ensure_abort(const char* condition, const char* file, size_t line) {
+  char msg[1024];
+
+  void *addrs[8] = { 0, 0, 0, 0, 0, 0, 0, 0 };
+  size_t count = k_get_call_stack(8, addrs);
+
+  snprintf(msg, sizeof(msg), "assert failed (%s:%d): %s\n"
+      "kernel panic: call stack: %p\n"
+      "                          %p\n"
+      "                          %p\n"
+      "                          %p\n"
+      "                          %p\n"
+      "                          %p\n"
+      "                          %p\n"
+      "                          %p\n", file, line, condition, addrs[0],
+      addrs[1], addrs[2], addrs[3], addrs[4], addrs[5], addrs[6], addrs[7]);
+
+  k_panic(msg);
+}
+
+/* http://yosefk.com/blog/getting-the-call-stack-without-a-frame-pointer.html */
--- kernel/kernel.h
+++ kernel/kernel.h
@@ -1,9 +1,12 @@
 #ifndef _KERNEL_H
 #define _KERNEL_H
 
-#define k_ensure(condition) \
-  if(!(condition)) {printf("assert failed(%s:%d): %s", __FILE__, __LINE__, #condition);while(1);}
+#include <stddef.h>
+
+#define k_ensure(condition) if(!(condition)) k_ensure_abort(#condition, __FILE__, __LINE__)
 
 void k_panic(const char* msg);
+size_t k_get_call_stack(size_t max_depth, void** return_addresses);
+void k_ensure_abort(const char* condition, const char* file, size_t line);
 
 #endif
